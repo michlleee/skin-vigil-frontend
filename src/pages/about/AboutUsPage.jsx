406414505b110db3b150aa8f8a53ee50e5062413
Michelle Glebova <147517074+michlleee@users.noreply.github.com>
2025-05-24 16:24:02 +0700

    minor fixes to about us page

--- src/pages/about/AboutUsPage.jsx
+++ src/pages/about/AboutUsPage.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import './AboutUsPage.css';
 import { Instagram, Twitter, Facebook } from 'lucide-react';
+import emailIcon from '../../assets/aboutUs/email.png'
+import phoneIcon from '../../assets/aboutUs/phone.png'
+import webIcon from '../../assets/aboutUs/website.png'
+import aboutUsImg from '../../assets/aboutUs/AboutUs.png'
 
 function AboutUsPage() {
   return (
     <div className="about-container">
       
       <div className="banner">
-        <img src="src/assets/aboutUs/AboutUs.png" alt="About Us Banner" />
+        <img src={aboutUsImg} alt="About Us Banner" />
       </div>
 
       <div className="content">
@@ -15,16 +19,16 @@ function AboutUsPage() {
         <div className="contact">
           <h2>Get In Touch</h2>
           <p>
-            Have questions or need assistance? We’re here to help! Feel free to reach out to us through
+            Have questions or need assistance? We're here to help! Feel free to reach out to us through
             any of the following channels, and our team will get back to you as soon as possible.
             Whether you have inquiries about our services, need support, or just want to learn more,
-            we’re always happy to assist.
+            we're always happy to assist.
           </p>
 
           <div className="contact-info">
             <div className="top-info">
               <div className="info-content">
-                <img src="src/assets/aboutUs/phone.png" alt="Phone" />
+                <img src={phoneIcon} alt="Phone" />
                 <span>
                   <strong>Phone</strong><br />
                   354-187-555
@@ -32,20 +36,20 @@ function AboutUsPage() {
               </div>
 
               <div className="info-content email-info">
-                <img src="src/assets/aboutUs/email.png" alt="Email" />
+                <img src={emailIcon} alt="Email" />
                 <span>
                   <strong>Email</strong><br />
-                  contact@yourwebsite.com
+                  skinvigil@gmail.com
                 </span>
               </div>
             </div>
 
             <div className="bottom-info">
               <div className="info-content">
-                <img src="src/assets/aboutUs/website.png" alt="Website" />
+                <img src={webIcon} alt="Website" />
                 <span>
                   <strong>Website</strong><br />
-                  www.yourwebsite.com
+                  www.skin-vigil.com
                 </span>
               </div>
             </div>
